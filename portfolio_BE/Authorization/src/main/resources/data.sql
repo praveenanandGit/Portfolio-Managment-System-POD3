950d59683d0cb672fabd3fb904db48706da8b403
Praveen Anand <48305458+praveenanandGit@users.noreply.github.com>
2022-08-03 19:26:56 +0530

    first commit
    first

---
+++ portfolio_BE/Authorization/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+drop table if exists userrecord;
+create table userrecord(user_id int primary key ,user_name varchar(30),user_password varchar(20));
+
+
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (101,'amruta','1234');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (102,'divya','1920');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (103,'sayan','1234');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (104,'monalisa','1234');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (105,'tejaswi','1234');
